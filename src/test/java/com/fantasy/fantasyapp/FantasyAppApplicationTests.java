ca794f0449664b2c56e200f2447af0e7158871a4
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2025-04-19 11:27:49 -0400

    starter files

---
+++ src/test/java/com/fantasy/fantasyapp/FantasyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fantasy.fantasyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FantasyAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
