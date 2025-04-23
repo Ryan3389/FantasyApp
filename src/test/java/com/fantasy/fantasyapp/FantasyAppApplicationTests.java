8d061e87ee927d5a7cf95b39c4ec394b365056e3
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2025-04-23 13:31:00 -0400

    added jar file

--- src/test/java/com/fantasy/fantasyapp/FantasyAppApplicationTests.java
+++ src/test/java/com/fantasy/fantasyapp/FantasyAppApplicationTests.java
@@ -3,7 +3,7 @@ package com.fantasy.fantasyapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class FantasyAppApplicationTests {
 
     @Test
