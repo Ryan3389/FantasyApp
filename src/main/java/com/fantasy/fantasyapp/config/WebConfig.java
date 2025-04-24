bef7f180c0dcb9eaa841cf35796839e572088109
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2025-04-24 17:37:47 -0400

    added url for cors config

--- src/main/java/com/fantasy/fantasyapp/config/WebConfig.java
+++ src/main/java/com/fantasy/fantasyapp/config/WebConfig.java
@@ -16,7 +16,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("https://baseball-fantasy.netlify.app/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
 
