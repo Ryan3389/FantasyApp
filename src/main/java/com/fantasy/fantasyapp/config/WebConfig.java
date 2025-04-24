dff9f76535b2cb7013e47cd6084ef0418bb045d5
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2025-04-24 18:01:27 -0400

    updated cors config

--- src/main/java/com/fantasy/fantasyapp/config/WebConfig.java
+++ src/main/java/com/fantasy/fantasyapp/config/WebConfig.java
@@ -1,8 +1,11 @@
 package com.fantasy.fantasyapp.config;
 
 
+import org.apache.catalina.filters.CorsFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,17 +13,31 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("https://baseball-fantasy.netlify.app/")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*");
 
-            }
-        };
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter();
     }
 }
+
+//@Bean
+//public WebMvcConfigurer corsConfigurer() {
+//    return new WebMvcConfigurer() {
+//        @Override
+//        public void addCorsMappings(CorsRegistry registry) {
+//            registry.addMapping("/**")
+//                    .allowedOrigins("https://baseball-fantasy.netlify.app/")
+//                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                    .allowedHeaders("*");
+//
+//
+//        }
+//    };
+//}
