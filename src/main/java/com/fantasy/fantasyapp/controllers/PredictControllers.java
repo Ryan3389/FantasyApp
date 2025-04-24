065bdb3502300044a684c2d36c2fa4bae0278a88
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2025-04-24 18:12:39 -0400

    addec cross origns for cors

--- src/main/java/com/fantasy/fantasyapp/controllers/PredictControllers.java
+++ src/main/java/com/fantasy/fantasyapp/controllers/PredictControllers.java
@@ -3,12 +3,10 @@ package com.fantasy.fantasyapp.controllers;
 import com.fantasy.fantasyapp.dto.PredictDTO;
 import com.fantasy.fantasyapp.service.PredictService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/predict")
 public class PredictControllers {
