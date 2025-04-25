33fe58f228fffe91418c23cb0dd695bf796eef13
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2025-04-24 21:38:38 -0400

    docker

--- src/main/java/com/fantasy/fantasyapp/FantasyAppApplication.java
+++ src/main/java/com/fantasy/fantasyapp/FantasyAppApplication.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @EnableJpaRepositories
 public class FantasyAppApplication {
+    public String PORT = System.getenv("PORT");
 
     public static void main(String[] args) {
         SpringApplication.run(FantasyAppApplication.class, args);
