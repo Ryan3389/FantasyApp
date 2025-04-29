171c23affa21810dd36055a1cc3aa9ab9efcc1b6
Ryan3389 <Ryan.Cuthbert12@gmail.com>
2025-04-29 14:31:09 -0400

    added python endpoint

---
+++ predict/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify
+import requests
+import pandas as pd
+import joblib
+from dotenv import load_dotenv
+import os
+
+app = Flask(__name__)
+
+model = joblib.load('hof_model.pkl')
+scaler = joblib.load('scaler.pkl')
+
+
+@app.route("/api/predict", methods=["POST"])
+def get_player_stats():
+    data = request.get_json()
+
+    player_stats = pd.DataFrame({
+        "YRS": [data['YRS']], # YRS
+        "G": [data['G']], # G
+        "H": [data['H']], # H
+        "2B": [data['doubles']], # 2B
+        "HR": [data['HR']], # HR
+        "RBI": [data['RBI']], # RBI
+        "BA": [data['BA']] # BA
+    })
+    scaled_player_stats = scaler.transform(player_stats)
+
+    model_prediction = model.predict(scaled_player_stats)
+
+    return jsonify("Hall of Fame: Yes" if model_prediction[0] == 1 else "Hall of Fame: No")
